ff9d8efada0312e1994860fc6e848735fe01a1f9
Jan <juan.perezlosa@gmail.com>
2022-02-24 21:15:36 +0100

    RxJs y página ver pais

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
+import { PaisService } from '../../services/pais.service';
+
 
 @Component({
   selector: 'app-ver-pais',
@@ -8,9 +13,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerPaisComponent implements OnInit {
 
-  constructor() { }
+  constructor( 
+    private activateRoute: ActivatedRoute,
+    private paisService: PaisService            
+    ) { }
 
   ngOnInit(): void {
+
+    this.activateRoute.params
+      .pipe(
+        switchMap( ( {id} ) => this.paisService.getPaisPorAlpha( id ) )
+      )
+      .subscribe( resp => {
+        console.log(resp);
+      });
+
+    //Otra forma
+    /* this.activateRoute.params
+      .subscribe( ({ id }) => {
+        console.log(id);
+
+        this.paisService.getPaisPorAlpha( id )
+          .subscribe( pais => {
+            console.log(pais)
+          });
+
+
+      }); */
+
   }
 
 }
